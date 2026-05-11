661dc31690d04d2d70fefa96fd8f13dc3c3af689
tobegreatman <41740663+tobegreatman@users.noreply.github.com>
2026-05-12 05:30:38 +0800

    Delete trade directory

--- trade/smart-stock/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router'
-import App from './App.vue'
-import './styles/main.css'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
